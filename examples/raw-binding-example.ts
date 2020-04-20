911e1fd91fcdfedbb92b75e01e0509f94bb16126
Abdessattar Sassi <abde.sassi@gmail.com>
2020-04-20 22:55:04 +0400

    fix: make harcoded paths and flags dynamic

--- examples/raw-binding-example.ts
+++ examples/raw-binding-example.ts
@@ -2,21 +2,30 @@
 // SPDX-License-Identifier: BSD-3-Clause
 
 import * as fs from 'fs';
+import * as path from 'path';
 import { MagicBindingModule, MagicBindingInterface } from '../lib/index';
 
 const bindingModule = require('../../dist/magic-js');
+
+const magicFile = path.normalize(
+  path.join(__dirname, '..', '..', 'dist', 'magic.mgc')
+);
+
 const binding: MagicBindingModule = bindingModule({
   onRuntimeInitialized() {
     console.log(binding);
     console.log(`Magic version : ${binding.MagicBinding.version()}`);
 
-    if (
-      -1 ===
-      binding.MagicBinding.init(
-        '/Users/abdessattar/Projects/maestro-magic/dist/magic.mgc',
-        binding.MAGIC_PRESERVE_ATIME
-      )
-    ) {
+    // We can only use MAGIC_PRESERVE_ATIME on operating suystems that support
+    // it and that includes OS X for example. It's a good practice as we don't
+    // want to change the last access time because we are just checking the file
+    // contents type
+    let flags = 0;
+    if (process.platform === 'darwin' || process.platform === 'linux') {
+      flags = binding.MAGIC_PRESERVE_ATIME;
+    }
+
+    if (-1 === binding.MagicBinding.init(magicFile, flags)) {
       console.error('Initialization failed!');
       return;
     }
